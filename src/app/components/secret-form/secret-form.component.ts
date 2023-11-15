5e0abdb2e7a617fec19fd6c539a0aa4fa3a268fb
Paulo de Tarso <potassio@hotmail.com>
2023-11-15 10:04:55 -0300

    Implantado o getheros nos forms

--- src/app/components/secret-form/secret-form.component.ts
+++ src/app/components/secret-form/secret-form.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Hero } from 'src/app/models/Hero';
+import { Secret } from 'src/app/models/Secret';
+import { HeroService } from 'src/app/service/hero.service';
+import { SecretService } from 'src/app/service/secret.service';
 
 @Component({
   selector: 'app-secret-form',
   templateUrl: './secret-form.component.html',
   styleUrls: ['./secret-form.component.css']
 })
-export class SecretFormComponent {
+export class SecretFormComponent implements OnInit {
 
+  heroes: Hero[] = [];
+
+  constructor(private heroService: HeroService, private secretService: SecretService) { }
+
+  ngOnInit(): void {
+    this.heroService.GetHeroes().subscribe(data => {
+      this.heroes = data;
+    });
+  }
 }
--- src/app/components/weapon-form/weapon-form.component.ts
+++ src/app/components/weapon-form/weapon-form.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Hero } from 'src/app/models/Hero';
+import { HeroService } from 'src/app/service/hero.service';
+import { SecretService } from 'src/app/service/secret.service';
 
 @Component({
   selector: 'app-weapon-form',
@@ -7,4 +10,13 @@ import { Component } from '@angular/core';
 })
 export class WeaponFormComponent {
 
+  heroes: Hero[] = [];
+
+  constructor(private heroService: HeroService, private secretService: SecretService) { }
+
+  ngOnInit(): void {
+    this.heroService.GetHeroes().subscribe(data => {
+      this.heroes = data;
+    });
+  }
 }
